c2f9b0864a6e2f7478d2b1c3f07c16cd264e738d
JGHoffmann <jandrey_hoffmann@hotmail.com>
2017-04-18 09:11:11 -0300

    Update Trabalho_1.v

--- Trabalho_1.v
+++ Trabalho_1.v
@@ -1,2 +1,16 @@
-# Sistemas-Digitais
-Trabalhos da Discplina de Sistemas Digitais
+module pisca (input clock, output led);
+  assign led = clock;
+endmodule
+
+module testclock;
+  reg clock;
+  wire led;
+  always #1 clock = ~clock;
+  pisca A(clock, led);
+  initial begin
+    $dumpvars(0, A);
+    clock <= 0;
+    #100;
+    $finish;
+  end
+endmodule
